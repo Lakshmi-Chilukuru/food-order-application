021f955117d1f77c78fb8eb9afb6189b43f8794f
Lakshmi Narayana Chilukuru <lakshminarayana.chilukuru@gmail.com>
2024-02-12 23:27:04 +0530

    added home component

--- src/app/navbar/home/home.component.ts
+++ src/app/navbar/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/model/food';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  
+  public foods:Food[]=[]
+  constructor(foodService:FoodService){
+    this.foods=foodService.getAll()   //get all data return from dummy data
+  }
+  ngOnInit(): void {
+    
+  }
 }
